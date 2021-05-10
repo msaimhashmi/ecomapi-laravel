332c1c6628262073329849b7aec6efaed646417c
saimhashmii <saimhashmii433@gmail.com>
2021-05-10 20:32:41 +0500

    Seeding with fake data!

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Product::factory(50)->create();
+        \App\Models\Review::factory(50)->create();
+        // factory(App\Models\Product::class, 50)->create();
+        // factory(App\Models\Review::class, 50)->create();
     }
 }
